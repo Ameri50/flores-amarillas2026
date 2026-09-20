cc19386681a01980b95d7bab4a019223bb37ac7b
Ameri50 <ssj52949@gmail.com>
2026-09-20 14:35:22 -0500

    dadad

---
+++ react/App.jsx
@@ -0,0 +1,13 @@
+import FloresAmarillas from "./FloresAmarillas";
+
+export default function App() {
+  return (
+    <FloresAmarillas
+      titulo="Hoy es tu día"
+      mensaje="Feliz 21 de marzo 🧡"
+      firma="@Ameri50"
+      flowerCount={22}
+      duracion={23}
+    />
+  );
+}
